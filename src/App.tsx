45014406b2cd8662e84113fbd6fa01cd77dcedd6
Nitish <tnitish654@gmail.com>
2024-01-16 23:03:09 +0800

    ⚡ Added bg and padding

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import { useEffect, useState } from "react"
 import QRCode from "react-qr-code"
 
 function App() {
-	const [url, setUrl] = useState("")
+    const [url, setUrl] = useState("")
 
-	useEffect(() => {
-		// @ts-expect-error chrome is not defined
-		chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-			setUrl(tabs[0].url)
-		})
-	}, [])
+    useEffect(() => {
+        // @ts-expect-error chrome is not defined
+        chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+            setUrl(tabs[0].url)
+        })
+    }, [])
 
-	return (
-		<div className="grid place-items-center size-[300px] ">{url && <QRCode value={url} />}</div>
-	)
+    return (
+        <div className="grid size-[300px] place-items-center">
+            <div className="size-fit rounded-xl bg-white p-4 shadow-xl shadow-indigo-300">
+                {url && <QRCode className="size-[225px]" value={url} />}
+            </div>
+        </div>
+    )
 }
 
 export default App
